60ca3afc351626c0fe7879dc75ab605ca6e6914f
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-09-19 19:04:04 -0500

    allow us to grab data on countries from supabase

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { getCountries } from '../../../services/client';
 import './Main.css';
 
 export default function Main() {
+  const country = getCountries();
+  console.log(country);
   return (
-    <main>Main</main>
+    <main>
+      Main
+    </main>
   );
 }
